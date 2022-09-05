80b464203e3a397a05a59bc2274409832eab8e80
Lauane Seixas <lauane.alves@magazineluiza.com.br>
2022-09-05 20:25:22 -0300

    Inicio Python

---
+++ INTRO/Aula7/Exercicio1.py
@@ -0,0 +1,6 @@
+#FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO E MOSTRE NA TELA O SEU SUCESSOR E SEU ANTECESSOR
+
+n1 = int(input('Digite um número: '))
+a = n1 - 1
+s = n1 + 1
+print('O antecessor do número digitado é: {} e o sucessor é: {}'.format(a, s))
